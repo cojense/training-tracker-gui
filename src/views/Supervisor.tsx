b265b55c8eb36c8201a99a17587d990911a512ae
Connor J. <connor.jensen@shyftsolutions.io>
2026-02-20 18:48:30 -0600

    DELETED OLD FILES NOT ON CURRENT BRANCH

--- src/views/Supervisor.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Supervisor: React.FC = () => (
-  <div>
-    <h1>Supervisor Report</h1>
-    <p>Reports for supervisors.</p>
-  </div>
-);
-export default Supervisor;
