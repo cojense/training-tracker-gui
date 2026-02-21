43f8954a507372b3b0a8f633982d541a67bd828a
Connor J. <connor.jensen@shyftsolutions.io>
2026-02-20 18:48:30 -0600

    DELETED OLD FILES NOT ON CURRENT BRANCH

--- src/views/Groups.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Groups: React.FC = () => (
-  <div>
-    <h1>Groups</h1>
-    <p>Group management.</p>
-  </div>
-);
-export default Groups;
--- src/views/Projects.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Projects: React.FC = () => (
-  <div>
-    <h1>Projects</h1>
-    <p>Project management.</p>
-  </div>
-);
-export default Projects;
